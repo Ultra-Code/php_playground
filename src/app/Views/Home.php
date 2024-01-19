33e6474372f0eebd668708e105bf9f1ef48f0f68
Ultra-Code <mega.alpha100@gmail.com>
2024-01-19 17:48:32 +0000

    use/execute pgsql in HOME

--- src/app/Views/Home.php
+++ src/app/Views/Home.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Playground\Views;
 
+use PDO;
+use PDOException;
+
 class Home
 {
     public function index(): string
@@ -18,7 +21,54 @@ class Home
         // echo PHP_EOL;
         //
         // debug_zval_dump($_REQUEST);
+        try {
+            $db = new PDO(dsn: "pgsql:host=pgsql;port=5432;dbname=default-db", username: "postgres", password:"postgress_password", options:[
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ]);
+            $db->setAttribute(attribute:PDO::ATTR_ERRMODE, value:PDO::ERRMODE_EXCEPTION);
 
+            $brand = $_GET["brand"];
+            $query = 'SELECT * FROM cars WHERE brand = ?';
+
+            // $statment = $db->query($query);
+            $statment = $db->prepare($query);
+            $statment->execute([$brand]);
+
+            // foreach ($db->query($query, PDO::FETCH_OBJ) as $user) {
+            //     echo "<pre>";
+            //     debug_zval_dump($user);
+            // }
+
+            foreach ($statment->fetchAll() as $user) {
+                echo "<pre>";
+                debug_zval_dump($user);
+            }
+
+            // debug_zval_dump($statment->fetchAll(PDO::FETCH_NUM));
+            $brand = "Hondai";
+            $model = "G15";
+            $year = "2018";
+            //place holder names for query parameter
+            //since there are no unique columns
+            $insert_query = 'INSERT INTO cars (brand, model, year) VALUES(:brand, :model, :year) ON CONFLICT DO NOTHING';
+            $insert_stmt = $db->prepare($insert_query);
+            $insert_stmt->execute(['brand' => $brand,'model' => $model,'year' => $year]);
+
+
+            $select_stmt = $db->prepare("SELECT * FROM cars WHERE brand = ?");
+            //select_stmt->bindValue(':brand',$brand) can be used to set values for the prepared statement or bindParam is you want to
+            //bind by reference
+            $select_stmt->execute([$brand]);
+
+            {   echo '<pre>';
+                $user = $select_stmt->fetch();
+                debug_zval_dump($user);
+            }
+
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
         $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
         echo "Home Page";
         return <<<FORM
