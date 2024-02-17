15962789a9effba0100e70fdaad038efc6b22ee7
Ultra-Code <mega.alpha100@gmail.com>
2024-02-17 17:59:56 +0000

    misc changes

--- src/app/fs.php
+++ src/app/fs.php
@@ -6,18 +6,18 @@ echo '<br/>';
 
 var_dump(is_dir($dir[3]));
 
-//rmdir() remove directory, mkdir() add directory
+// rmdir() remove directory, mkdir() add directory
 
-//file_exists() checks the existance of a file
-//filesize , file_put_contents put content into a file
+// file_exists() checks the existance of a file
+// filesize , file_put_contents put content into a file
 //
-//some functions like filesize cache result but you can use clearstatcache to ensure you get the latest result
+// some functions like filesize cache result but you can use clearstatcache to ensure you get the latest result
 
-$file = fopen('txt', 'r'); // to read content of file
+$file = fopen('txt', 'r');  // to read content of file
 while (($line = fgets($file)) !== false) {
-    echo $line  . '<br/>';
+    echo $line.'<br/>';
 }
 
 fclose($file);
 
-//gets and stores the content of a file in file_get_contents() into a string
+// gets and stores the content of a file in file_get_contents() into a string
--- src/app/time.php
+++ src/app/time.php
@@ -2,46 +2,45 @@
 
 namespace Ultracode\Test;
 
-require "utils.php";
+require 'utils.php';
 
-echo time() . '<br/>';
+echo time().'<br/>';
 
-echo date("M/D/Y g:ia") . '<br/>';
+echo date('M/D/Y g:ia').'<br/>';
 
-echo date_default_timezone_get() . '<br/>';
+echo date_default_timezone_get().'<br/>';
 
-$date = date("M/D/Y g:ia", strtotime("2011-11-11 11:11:11")) . '<br/>';
+$date = date('M/D/Y g:ia', strtotime('2011-11-11 11:11:11')).'<br/>';
 
 echo '<pre>';
 print_r(date_parse($date));
 echo '</pre>';
 
 echo '<pre>';
-print_r(date_parse_from_format("M/D/Y g:ia", $date));
+print_r(date_parse_from_format('M/D/Y g:ia', $date));
 echo '</pre>';
-
 
 $items = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => '6'];
 pretty_print(array_chunk($items, 2));
 pretty_print(array_chunk($items, 2, true));
 
-$array = [1,2,3,4,5,6,7,8,9];
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // keys aren't reindexed so there might be gaps in the keys
 $even = array_filter($array, fn ($element) => $element % 2 === 0, ARRAY_FILTER_USE_BOTH);
 pretty_print($even);
-//to reindexed the array
+// to reindexed the array
 $even = array_values($even);
 pretty_print($even);
 
 // array_search() or in_array() can be use to search for an element in an array
 
-//array_diff in elements in the first array that aren't in any of the rest of the arrays
+// array_diff in elements in the first array that aren't in any of the rest of the arrays
 
 // asort fn ksort fn usort fn for sorting arrays
-//rsort($array) or sort($array, SORT_DESC) can also be used
-usort($array, fn ($left, $right) => $right <=> $left); //reverse sorting
+// rsort($array) or sort($array, SORT_DESC) can also be used
+usort($array, fn ($left, $right) => $right <=> $left);  // reverse sorting
 pretty_print($array);
 
-$array = [1,2,[3,4]];
-[$a, $b,[ $c, $d]] = $array;
-echo $a. ' , ' . $b . ' , ' . $c . ' , ' . $d;
+$array = [1, 2, [3, 4]];
+[$a, $b, [$c, $d]] = $array;
+echo $a.' , '.$b.' , '.$c.' , '.$d;
