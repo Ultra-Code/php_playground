ac2436c699b2f5a392466734a816032f21fb7f21
Ultra-Code <mega.alpha100@gmail.com>
2024-02-26 17:27:27 +0000

    Improve typing, still needs more work

--- lara/app/Models/Post.php
+++ lara/app/Models/Post.php
@@ -6,17 +6,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Post
 {
-    public static function all(): array<array-key,string>
+    /**
+     * @return string[]
+     */
+    public static function all(): array
     {
         $files = File::files(resource_path('html/'));
 
-        return array_map(fn ($file) => $file->getContents(), $files);
+        return array_map(static function (SplFileInfo $file): string {
+            return (string) file_get_contents($file->getPathname());
+        }, $files);
     }
 
-    public static function find(string $route_wildcard): string
+    /**
+     * Get an item from the cache, or execute the given Closure and store the result.
+     *
+     * @template TCacheValue
+     *
+     * @param  string  $route_wildcard
+     * @return string|TCacheValue
+     */
+    public static function find(string $route_wildcard)
     {
         if (! file_exists($path = resource_path("html/{$route_wildcard}.html"))) {
             throw new ModelNotFoundException();
